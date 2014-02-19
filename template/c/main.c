c0765c3f4b651bb739e38b8a723dc1162fa53438
王茂松 <song11210312@126.com>
2014-02-19 18:13:01 +0800

    load_template

---
+++ template/c/main.c
@@ -0,0 +1,17 @@
+#include <errno.h>
+#include <math.h>  
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+
+	return 0;
+}
